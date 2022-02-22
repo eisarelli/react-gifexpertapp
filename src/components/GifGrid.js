4cc63a8377095114c60f102097eff41e63e82778
Esteban Isarelli <esteban.isarelli@gmail.com>
2022-02-22 12:35:38 -0300

    First commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GiftGridItem } from './GiftGridItem';
+
+export const GifGrid = ( { category } ) => {
+
+    const { data:images, loading } = useFetchGifs( category );
+
+    return (
+        <>
+            <h3> { category } </h3>
+
+            { loading ? <p className='card animate__animated animate__flash'>Loading</p> : null }
+            <div className='card-grid'>
+                
+                {
+                    images.map( img => (
+                        <GiftGridItem 
+                            key={ img.id }
+                            { ...img }
+                        />
+                    )) 
+                }
+            </div>
+        </>
+    )
+}
